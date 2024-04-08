6e85900700ec538299475cf701eb36f73a63a43b
cbensenai <165176254+cbensenai@users.noreply.github.com>
2024-04-08 10:06:59 -0300

    segundo commit

---
+++ teste.py
@@ -0,0 +1,9 @@
+class Aluno:
+    def __init__(self, nome, sexo, idade, matricula):
+        self.nome = nome
+        self.idade = idade
+        self.sexo = sexo
+        self.matricula = matricula
+
+if __name__ == '__main__':
+    
